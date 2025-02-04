64614ea7ffa991c17a1f1a6cc34ab6456f007f2b
shaima1256sha <shaimahoushati@gmail.com>
2025-02-05 00:27:47 +0100

    Episode 02 : Igntting Our App

--- App.js
+++ App.js
@@ -1,27 +1,32 @@
-const heading = React.createElement(
-  "h1",
-  { id: "headings", xyz: "khnifs" },
-  "hello from js"
-);
-console.log(heading); //return object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "headings", xyz: "bachboucha" },
+//   "hello from js"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
 
 const parent = React.createElement(
   "div",
-  { id: "parent" },[
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "i am h1 tag"),
-    React.createElement("h2", {}, "i am h2 tag"),
-  ])],
+  { id: "parent" },
   [
     React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "learn parcel"),
+      React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+  ],
+  [
+    React.createElement("div", { id: "child2" }, [
       React.createElement("h1", {}, "i am h1 tag"),
       React.createElement("h2", {}, "i am h2 tag"),
-    ])]
+    ]),
+  ]
 );
 
-const secpart = ReactDOM.createRoot(document.getElementById("secpart"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-secpart.render(parent);
+root.render(parent);
 console.log(parent);
---
+++ README.md
@@ -0,0 +1,20 @@
+# Pingucoder React
+
+# Parrce
+- Dev Build
+- Local server
+- HMR = hot module replacement
+- File watching algorithm written with c++(y7ot 3inou 3la ay changement)
+- caching - faster build
+- Image optimization (taya7 f taille d'image w qualite heyya bidha )
+- Minifiction
+- Bundling (yajma3)
+- Compress
+- Consisting Hashing (in system design)
+- Code splitting(kol 7aja wa7dha fl code "js,html..")
+- Diffirential Bundling( support old browsers)
+- Diagnostic (error showing exactly)
+- Error handling 
+- support HTTPS
+- Tree shaking ( remove unused code "dist")
+- Different between dev and prod bundling (prod take more time)
